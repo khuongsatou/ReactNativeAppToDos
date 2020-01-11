0ffbc18b1c49e6f3d53ee4613382e4f80c88b162
khuongsatou <yoshi240499@gmail.com>
2020-01-11 12:41:18 +0700

    Update Onpress KeyBoard Android

--- App.js
+++ App.js
@@ -1,51 +1,55 @@
-import React,{ useState } from 'react';
-import { StyleSheet, Text, View, FlatList,Alert } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header.js';
 import ToDoItem from './components/toDoItem.js';
 import AddToDo from './components/addToDo.js';
 export default function App() {
-  const[todos,setTodos] = useState([
+  const [todos, setTodos] = useState([
     { text: 'buy coffee', key: '1' },
     { text: 'create an app', key: '2' },
     { text: 'play on the switch', key: '3' },
   ]);
 
-  const pressHandler = (key)=>{
+  const pressHandler = (key) => {
     setTodos((prevToDos) => {
-      return prevToDos.filter(todo => todo.key !=key);
+      return prevToDos.filter(todo => todo.key != key);
     })
   }
   const submitHandler = (text) => {
-    if(text.length > 3 ){
-      setTodos((prevToDos)=>{
+    if (text.length > 3) {
+      setTodos((prevToDos) => {
         return [
-          { text:text, key: Math.random().toString() },
+          { text: text, key: Math.random().toString() },
           ...prevToDos
         ]
       });
-    }else{
-      Alert.alert('OOPS!','toDos',[
-        {text:'OK',onPress:()=>console.log('alert Closed')}
+    } else {
+      Alert.alert('OOPS!', 'toDos', [
+        { text: 'OK', onPress: () => console.log('alert Closed') }
       ]);
     }
   }
 
 
   return (
-    <View style={styles.container}>
-      {/* header */}
-      <Header/>
-      <View style={styles.content}></View>
-      {/* to form */}
-      <AddToDo submitHandler={submitHandler}/>
-      <View style={styles.list}></View>
-      <FlatList
-        data={todos}
-        renderItem={({item}) => (
-          <ToDoItem item={item} pressHandler={pressHandler}/>
-        )}
-      />
-    </View>
+    <TouchableWithoutFeedback onPress={() => {
+      console.log("Dismissed ketboard");
+    }}>
+      <View style={styles.container}>
+        {/* header */}
+        <Header />
+        <View style={styles.content}></View>
+        {/* to form */}
+        <AddToDo submitHandler={submitHandler} />
+        <View style={styles.list}></View>
+        <FlatList
+          data={todos}
+          renderItem={({ item }) => (
+            <ToDoItem item={item} pressHandler={pressHandler} />
+          )}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -56,10 +60,10 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     justifyContent: 'center',
   },
-  content:{
-    padding:40,
+  content: {
+    padding: 40,
   },
-  list:{
-    marginTop:20,
+  list: {
+    marginTop: 20,
   }
 });
